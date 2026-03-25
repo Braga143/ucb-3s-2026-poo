656a337f44ec1f96335b1e357458847d7ab60dca
WALISSON ALVES BRAGA <walisson.braga@a.ucb.br>
2026-03-24 21:27:32 -0300

    Codigo identado

--- projetos_poo_2026/Estoque/src/main/java/com/mycompany/estoque/TestaProduto.java
+++ projetos_poo_2026/Estoque/src/main/java/com/mycompany/estoque/TestaProduto.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.estoque;
 
 /**
@@ -9,15 +5,15 @@ package com.mycompany.estoque;
  * @author walisson.braga
  */
 public class TestaProduto {
-    
+
     //criar o metodo cosntrutor 
     public static void main(String[] args) {
-       Produto p1 = new Produto("Monitor 24' ", 900, 10);
-       Produto p2 = new Produto("CPU ", 1500, 10);
-       Produto p3 = new Produto("Mause ", 80, 10);
-       
-       //Exibir informações
-       p1.exibirInformacoes();
+        Produto p1 = new Produto("Monitor 24' ", 900, 10);
+        Produto p2 = new Produto("CPU ", 1500, 10);
+        Produto p3 = new Produto("Mause ", 80, 10);
+
+        //Exibir informações
+        p1.exibirInformacoes();
         System.out.println("********************");
         p2.exibirInformacoes();
         System.out.println("********************");
